2f7b9380a6964227bde8e8a45e18a6f4d2d2cbb5
Bertil Muth <bertilmuth@yahoo.de>
2022-06-10 10:44:25 +0200

    Get rid of superfluous arguments and methods

--- src/test/java/org/requirementsascode/act/statemachine/testdata/FlatCartState.java
+++ src/test/java/org/requirementsascode/act/statemachine/testdata/FlatCartState.java
@@ -6,15 +6,12 @@ import java.util.List;
 
 import org.requirementsascode.act.statemachine.testdata.trigger.AddItem;
 import org.requirementsascode.act.statemachine.testdata.trigger.RemoveItem;
-import org.requirementsascode.act.statemachine.testdata.trigger.Trigger;
 
 public class FlatCartState {
 	private final List<String> items;
-	private final boolean subStateEntered;
 	
-	private FlatCartState(List<String> items, boolean subStateEntered) {		
+	private FlatCartState(List<String> items) {		
 		this.items = new ArrayList<>(items);
-		this.subStateEntered = subStateEntered;
 	}
 	
 	public boolean isEmpty() {
@@ -25,44 +22,28 @@ public class FlatCartState {
 		return Collections.unmodifiableList(items);
 	}
 	
-	public boolean isSubStateEntered() {
-		return subStateEntered;
-	}
-	
-	static FlatCartState cartState(List<String> items, boolean subStateEntered) {
-		return new FlatCartState(items, subStateEntered);
+	static FlatCartState cartState(List<String> items) {
+		return new FlatCartState(items);
 	}
 	
 	static FlatCartState createCart(CreateCart createCart) {
-		return cartState(createCart.getItems(), createCart.isSubStateEntered());
+		return cartState(createCart.getItems());
 	}
 	
 	FlatCartState addItem(AddItem addItem) {		
 		ArrayList<String> items = new ArrayList<>(getItems());
 		items.add(addItem.item());
-		return cartState(items, subStateEntered);
+		return cartState(items);
 	}
 
 	FlatCartState removeItem(RemoveItem removeItem) {		
 		ArrayList<String> items = new ArrayList<>(getItems());
 		items.remove(removeItem.item());
-		return cartState(items, subStateEntered);
-	}
-	
-	FlatCartState removeAllItems(AddItem removeAllItems) {				
-		return cartState(Collections.emptyList(), false);
-	}
-	
-	FlatCartState enterSubstate(Trigger trigger) {
-		return cartState(getItems(), true);
-	}
-	
-	FlatCartState exitSubstate(Trigger trigger) {
-		return cartState(getItems(), false);
+		return cartState(items);
 	}
 
 	@Override
 	public String toString() {
-		return "CartState [items=" + items + ", subStateEntered=" + subStateEntered + "]";
+		return "FlatCartState [items=" + items + "]";
 	}
 }
