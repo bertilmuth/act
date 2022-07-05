686d9ba26db4f5f59612ae653f968de6935d9675
Bertil Muth <bertilmuth@yahoo.de>
2022-07-05 22:53:35 +0200

    Update RegularLanguageTest.java

--- src/test/java/org/requirementsascode/act/statemachine/RegularLanguageTest.java
+++ src/test/java/org/requirementsascode/act/statemachine/RegularLanguageTest.java
@@ -2,9 +2,11 @@ package org.requirementsascode.act.statemachine;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.requirementsascode.act.core.Data.data;
 import static org.requirementsascode.act.core.InCase.inCase;
 import static org.requirementsascode.act.statemachine.State.state;
 import static org.requirementsascode.act.statemachine.Transition.transition;
+import static org.requirementsascode.act.statemachine.RegularLanguageTest.NonTerminal.*;
 
 import java.util.function.Predicate;
 
@@ -13,6 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
 
+/**
+ * Test for a regular language that accepts words of exactly three letters of a and b character,
+ * where the last letter must be b.
+ * 
+ * @author b_muth
+ *
+ */
 class RegularLanguageTest {
 	private Statemachine<NonTerminal, String> statemachine;
 
@@ -23,49 +32,60 @@ class RegularLanguageTest {
 
 	@Test
 	void acceptsThreeLetterStringWithLastOneBeingB() {
-		Data<NonTerminal, String> input = Data.data(NonTerminal.S0, "bbb");
+		Data<NonTerminal, String> input = data(S0, "bbb");
 		Data<NonTerminal, String> output = statemachine.actOn(input);
 		assertTrue(output.getState().isAccepting());
 	}
 	
 	@Test
 	void acceptsAnotherThreeLetterStringWithLastOneBeingB() {
-		Data<NonTerminal, String> input = Data.data(NonTerminal.S0, "aab");
+		Data<NonTerminal, String> input = data(S0, "aab");
 		Data<NonTerminal, String> output = statemachine.actOn(input);
 		assertTrue(output.getState().isAccepting());
 	}
 	
 	@Test
+	void rejectsThreeLetterStringWithLastOneBeingB_ifFirstLetterIsNeitherANorB() {
+		Data<NonTerminal, String> input = data(S0, "cab");
+		Data<NonTerminal, String> output = statemachine.actOn(input);
+		assertFalse(output.getState().isAccepting());
+	}
+	
+	@Test
 	void rejectsFourLetterStringWithLastOneBeingB() {
-		Data<NonTerminal, String> input = Data.data(NonTerminal.S0, "bbbb");
+		Data<NonTerminal, String> input = data(S0, "bbbb");
 		Data<NonTerminal, String> output = statemachine.actOn(input);
 		assertFalse(output.getState().isAccepting());
 	}
 	
 	@Test
 	void rejectsEmptyString() {
-		Data<NonTerminal, String> input = Data.data(NonTerminal.S0, "");
+		Data<NonTerminal, String> input = data(S0, "");
 		Data<NonTerminal, String> output = statemachine.actOn(input);
 		assertFalse(output.getState().isAccepting());
 	}
+	
+	////////////////////////////////////////
+	// Test state machine data starts here /
+	////////////////////////////////////////
 
 	private Statemachine<NonTerminal, String> createStatemachine() {
-		State<NonTerminal, String> s0 = s("s0", NonTerminal.S0);
-		State<NonTerminal, String> s1 = s("s1", NonTerminal.S1);
-		State<NonTerminal, String> s2 = s("s2", NonTerminal.S2);
-		State<NonTerminal, String> s3 = s("s3", NonTerminal.S3);
+		State<NonTerminal, String> s0 = s(S0);
+		State<NonTerminal, String> s1 = s(S1);
+		State<NonTerminal, String> s2 = s(S2);
+		State<NonTerminal, String> s3 = s(S3);
 
 		Statemachine<NonTerminal, String> statemachine = Statemachine.builder()
 			.states(s0,s1, s2, s3)
 			.transitions(
-				transition(s0, s1, accept("a", NonTerminal.S1)),
-				transition(s0, s1, accept("b", NonTerminal.S1)),
-				transition(s1, s2, accept("a", NonTerminal.S2)),
-				transition(s1, s2, accept("b", NonTerminal.S2)),
-				transition(s2, s3, accept("b", NonTerminal.S3))
+				transition(s0, s1, accept("a", S1)),
+				transition(s0, s1, accept("b", S1)),
+				transition(s1, s2, accept("a", S2)),
+				transition(s1, s2, accept("b", S2)),
+				transition(s2, s3, accept("b", S3))
 			)
 			.flows(
-				ExitFlow.exitFlow(s3, inCase(i -> !i.getValue().isEmpty(), Transit.transit((s,t) -> NonTerminal.Rejected)))
+				ExitFlow.exitFlow(s3, inCase(i -> !i.getValue().isEmpty(), Transit.transit((s,t) -> Rejected)))
 			)
 			.build();
 		
@@ -86,21 +106,29 @@ class RegularLanguageTest {
 		}
 	};
 	
-	private State<NonTerminal, String> s(String stateName, NonTerminal nonTerminal){
-		// The Behavior.identity() part is the state's behavior.
-		// The purpose is to pass on the output value of each transition that fires 
+	///////////////////////////////////////////////////
+	// The regular language specific code starts here /
+	///////////////////////////////////////////////////
+	
+	private State<NonTerminal, String> s(NonTerminal nonTerminal){
+		// The Behavior.identity() part is the state behavior.
+		// The purpose of the state behavior is to pass on the output value of each transition that fires 
 		// to another transition, unchanged.
-		return state(stateName, stateEquals(nonTerminal), Behavior.identity());
+		return state(nonTerminal.toString(), stateInvariantOf(nonTerminal), Behavior.identity());
 	}
 	
-	private Predicate<NonTerminal> stateEquals(NonTerminal nonTerminal) {
+	private Predicate<NonTerminal> stateInvariantOf(NonTerminal nonTerminal) {
 		return nonTerminal::equals;
 	}
 	
-	private Behavior<NonTerminal, String> accept(String terminal, NonTerminal target) {
+	private Behavior<NonTerminal, String> accept(String terminal, NonTerminal targetState) {
 		return inCase(i -> 
-		  !i.getValue().isEmpty() && firstLetter(terminal) == firstLetter(i.getValue()), 
-		  	i -> Data.data(target, tail(i.getValue())));
+		  isFirstLetterTheSame(terminal, i), 
+		  	i -> data(targetState, tail(i.getValue())));
+	}
+
+	private boolean isFirstLetterTheSame(String terminal, Data<NonTerminal, String> i) {
+		return !i.getValue().isEmpty() && firstLetter(terminal) == firstLetter(i.getValue());
 	}
 
 	private char firstLetter(String s) {
