8a504ab5854e7efb7087495e4b7eca3a58a4206d
Bertil Muth <bertilmuth@yahoo.de>
2022-08-10 09:17:23 +0200

    Change names from input/output to before/after

--- src/main/java/org/requirementsascode/act/core/Behavior.java
+++ src/main/java/org/requirementsascode/act/core/Behavior.java
@@ -4,18 +4,18 @@ import static org.requirementsascode.act.core.Change.change;
 import java.util.Objects;
 
 public interface Behavior<S, V> {
-	Data<S,V> actOn(Data<S,V> input);
+	Data<S,V> actOn(Data<S,V> before);
 
 	default Behavior<S, V> andThen(Behavior<S, V> nextBehavior){
     Objects.requireNonNull(nextBehavior);    
-    return input -> nextBehavior.actOn(actOn(input));
+    return before -> nextBehavior.actOn(actOn(before));
 	}
 	
 	default Behavior<S, V> andHandleChangeWith(HandleChangeWith<S, V> changeHandler){
-		return input -> {
-			Data<S, V> output = actOn(input);
-			changeHandler.handleChangeWith(change(input, output));
-			return output;
+		return before -> {
+			Data<S, V> after = actOn(before);
+			changeHandler.handleChangeWith(change(before, after));
+			return after;
 		};
 	}
 	
