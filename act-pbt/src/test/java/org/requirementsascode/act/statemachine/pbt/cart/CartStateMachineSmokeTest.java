1f0286c39b5d2a6e48e353bf5947a004f5b39bea
Bertil Muth <bertilmuth@yahoo.de>
2022-08-17 00:10:43 +0200

    Rework example based test

--- act-pbt/src/test/java/org/requirementsascode/act/statemachine/pbt/cart/CartStateMachineSmokeTest.java
+++ act-pbt/src/test/java/org/requirementsascode/act/statemachine/pbt/cart/CartStateMachineSmokeTest.java
@@ -1,10 +1,16 @@
 package org.requirementsascode.act.statemachine.pbt.cart;
 
+import static java.util.Collections.emptyList;
+
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.requirementsascode.act.core.Data.data;
+import static org.requirementsascode.act.statemachine.pbt.cart.Cart.cart;
 
+import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.requirementsascode.act.core.Data;
 import org.requirementsascode.act.statemachine.pbt.cart.Cart.AddItem;
@@ -13,41 +19,34 @@ import org.requirementsascode.act.statemachine.pbt.cart.Cart.RemoveItem;
 import org.requirementsascode.act.statemachine.pbt.cart.Cart.Value;
 
 class CartStateMachineSmokeTest {
+	private CartStateMachine statemachine;
+
+	@BeforeEach
+	void setup() {
+		statemachine = new CartStateMachine();
+	}
 
 	@Test
-	void test() {
-		CartStateMachine statemachine = new CartStateMachine();
-		Data<Cart,Value> create = data(null, new CreateCart());
-		var after = statemachine.actOn(create);
+	void createsEmptyCart() {
+		Data<Cart, Value> createFromScratch = data(null, new CreateCart());
+		var after = statemachine.actOn(createFromScratch);
 		assertThat(cartSize(after)).isEqualTo(0);
-		
-		String testString = "AAAAA";
-		
-		Data<Cart,Value> add = data(after.state(), new AddItem(testString));
-		after = statemachine.actOn(add);
+	}
+
+	@Test
+	void addsOneElement() {
+		final String testString = "AAAAA";
+		Data<Cart, Value> add = data(cart(emptyList()), new AddItem(testString));
+		Data<Cart, Value> after = statemachine.actOn(add);
 		assertThat(cartSize(after)).isEqualTo(1);
 		assertThat(items(after).get(0)).isEqualTo(testString);
-		
-		add = data(after.state(), new AddItem(testString));
-		after = statemachine.actOn(add);
-		assertThat(cartSize(after)).isEqualTo(2);
-		assertThat(items(after).get(1)).isEqualTo(testString);
-		
-		add = data(after.state(), new AddItem(testString));
-		after = statemachine.actOn(add);
-		assertThat(cartSize(after)).isEqualTo(3);
-		assertThat(items(after).get(2)).isEqualTo(testString);
-		
-		Data<Cart,Value> remove = data(after.state(), new RemoveItem(testString));
-		after = statemachine.actOn(remove);
-		assertThat(cartSize(after)).isEqualTo(2);
-		
-		remove = data(after.state(), new RemoveItem(testString));
-		after = statemachine.actOn(remove);
-		assertThat(cartSize(after)).isEqualTo(1);
-		
-		remove = data(after.state(), new RemoveItem(testString));
-		after = statemachine.actOn(remove);
+	}
+	
+	@Test
+	void removesOneElement() {
+		final String testString = "AAAAA";
+		Data<Cart, Value> add = data(cart(asList(testString)), new RemoveItem(testString));
+		Data<Cart, Value> after = statemachine.actOn(add);
 		assertThat(cartSize(after)).isEqualTo(0);
 	}
 
@@ -58,5 +57,4 @@ class CartStateMachineSmokeTest {
 	private int cartSize(Data<Cart, Value> data) {
 		return items(data).size();
 	}
-
 }
