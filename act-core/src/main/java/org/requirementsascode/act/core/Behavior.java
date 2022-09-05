72dbc8c1b49902a79a64f436ea81acbd1581d802
Bertil Muth <bertilmuth@yahoo.de>
2022-09-05 12:06:11 +0200

    Introduce null checks

--- act-core/src/main/java/org/requirementsascode/act/core/Behavior.java
+++ act-core/src/main/java/org/requirementsascode/act/core/Behavior.java
@@ -8,11 +8,12 @@ public interface Behavior<S, V1, V2> {
 	Data<S,V2> actOn(Data<S,V1> before);
 
 	default <V3> Behavior<S, V1, V3> andThen(Behavior<S, V2, V3> nextBehavior){
-    Objects.requireNonNull(nextBehavior);    
+    Objects.requireNonNull(nextBehavior, "nextBehavior must be non-null!");    
     return before -> nextBehavior.actOn(actOn(before));
 	}
 	
 	default Behavior<S, V1, V2> andHandleChangeWith(HandleChange<S, V1, V2> changeHandler){
+    Objects.requireNonNull(changeHandler, "changeHandler must be non-null!");    
 		return before -> {
 			Data<S, V2> after = actOn(before);
 			Change<S, V1, V2> change = change(before, after);
