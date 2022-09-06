df82c2a7eada4e10f20213ed89d1e44e1f69a409
Bertil Muth <bertilmuth@yahoo.de>
2022-09-06 12:17:10 +0200

    Make Flow interface extend AsBehavior interface

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Flow.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Flow.java
@@ -1,5 +1,10 @@
 package org.requirementsascode.act.statemachine;
 
-public interface Flow<S, V0>{
+import org.requirementsascode.act.core.Behavior;
+
+public interface Flow<S, V0> extends AsBehavior<S, V0>{
 	Transition<S, V0> asTransition(Statemachine<S,V0> owningStatemachine);
+	default Behavior<S, V0, V0> asBehavior(Statemachine<S, V0> owningStatemachine){
+		return asTransition(owningStatemachine).asBehavior(owningStatemachine);
+	}
 }
