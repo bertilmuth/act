4b289bd1351d67d6b3fe37b767584b87b4610650
Bertil Muth <bertilmuth@yahoo.de>
2022-09-24 21:21:44 +0200

    Introduce event class

--- act-statemachine/src/test/java/org/requirementsascode/act/statemachine/ToStateBehaviorTest.java
+++ act-statemachine/src/test/java/org/requirementsascode/act/statemachine/ToStateBehaviorTest.java
@@ -15,28 +15,34 @@ class ToStateBehaviorTest {
 	private static final String S2 = "s2";
 
 	private boolean s2Entered = false;
-	private Statemachine<String, String> statemachine;
+	private Statemachine<String, Trigger> statemachine;
 	
 	@BeforeEach
 	void setup() {
-		State<String, String> s1 = State.state(S1, s -> S1.equals(s));
-		State<String, String> s2 = State.state(S2, s -> S2.equals(s));
+		State<String, Trigger> s1 = State.state(S1, s -> S1.equals(s));
+		State<String, Trigger> s2 = State.state(S2, s -> S2.equals(s));
 		
 		statemachine = Statemachine.builder()
 				.states(s1, s2)
 				.transitions(
-						transition(s1,s2, consumeWith((state, str) -> {
-							s2Entered = true;
-							return S2;
-						}))
-					)
+						transition(s1,s2, 
+							when(EnterS2.class, consumeWith(this::enterS2))
+						))
 				.build();
 	}
 
 	@Test
 	void entersS2() {
-		Data<String, String> result = statemachine.actOn(data(S1, "event"));
+		Data<String, Trigger> result = statemachine.actOn(data(S1, new EnterS2()));
 		assertThat(result.state()).isEqualTo(S2);
 		assertThat(s2Entered).isEqualTo(true);
+	}
+	
+	interface Trigger {}
+	class EnterS2 implements Trigger{};
+	
+	String enterS2(String state, EnterS2 event) {
+		s2Entered = true;
+		return S2;
 	}
 }
