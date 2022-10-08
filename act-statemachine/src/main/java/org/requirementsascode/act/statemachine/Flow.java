f4071550cd81a60a47cb00290eef4456b187cb40
Bertil Muth <bertilmuth@yahoo.de>
2022-10-08 14:14:24 +0200

    Make Flow independent of AsBehavior

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Flow.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Flow.java
@@ -1,10 +1,5 @@
 package org.requirementsascode.act.statemachine;
 
-import org.requirementsascode.act.core.Behavior;
-
-public interface Flow<S, V0> extends AsBehavior<S, V0>{
+public interface Flow<S, V0>{
 	Transition<S, V0> asTransition(Statemachine<S,V0> owningStatemachine);
-	default Behavior<S, V0, V0> asBehavior(Statemachine<S, V0> owningStatemachine){
-		return asTransition(owningStatemachine).asBehavior(owningStatemachine);
-	}
 }
