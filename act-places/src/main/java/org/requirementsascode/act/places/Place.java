f2041413affe010679b37e17a0b4d0f3d70469f7
Bertil Muth <bertilmuth@yahoo.de>
2022-10-10 16:23:28 +0200

    Tests for putting token on place

--- act-places/src/main/java/org/requirementsascode/act/places/Place.java
+++ act-places/src/main/java/org/requirementsascode/act/places/Place.java
@@ -1,9 +1,13 @@
 package org.requirementsascode.act.places;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.requirementsascode.act.statemachine.State;
 
 public class Place<S, V> {
 	private State<S,V> state;
+	private V token;
 
 	public Place(State<S, V> state) {
 		this.state = state;
@@ -12,9 +16,16 @@ public class Place<S, V> {
 	public static <S,V> Place<S, V> forState(State<S, V> state) {
 		return new Place<>(state);
 	}
+	
+	public void put(V token) {	
+		this.token = token;
+	}
 
 	public State<S,V> state() {
 		return state;
 	}
 
+	public List<V> tokens() {
+		return Arrays.asList(token);
+	}
 }
--- act-places/src/test/java/org/requirementsascode/act/places/PlaceTest.java
+++ act-places/src/test/java/org/requirementsascode/act/places/PlaceTest.java
@@ -13,4 +13,20 @@ class PlaceTest {
 		Place<String,String> place = Place.forState(state1);
 		assertEquals(state1, place.state());
 	}
+	
+	@Test
+	void putsTokenOnPlace() {
+		State<String, String> state1 = state("State1", s -> true);
+		Place<String,String> place = Place.forState(state1);
+		place.put("Token1");
+		assertEquals("Token1", place.tokens().get(0));
+	}
+	
+	@Test
+	void putsDifferentTokenOnPlace() {
+		State<Number, Number> state1 = state("State1", s -> true);
+		Place<Number,Number> place = Place.forState(state1);
+		place.put(5);
+		assertEquals(5, place.tokens().get(0));
+	}
 }
