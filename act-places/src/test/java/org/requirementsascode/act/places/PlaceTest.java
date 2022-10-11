a0178d969b851b1018bf975d417282960b5663ab
Bertil Muth <bertilmuth@yahoo.de>
2022-10-11 10:44:56 +0200

    Refactor test

--- act-places/src/test/java/org/requirementsascode/act/places/PlaceTest.java
+++ act-places/src/test/java/org/requirementsascode/act/places/PlaceTest.java
@@ -2,6 +2,7 @@ package org.requirementsascode.act.places;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.requirementsascode.act.statemachine.StatemachineApi.state;
 
 import org.junit.jupiter.api.Test;
@@ -11,34 +12,34 @@ class PlaceTest {
 	@Test
 	void createsPlaceForState() {
 		State<String, String> state1 = state("State1", s -> true);
-		Place<String,String> place = Place.forState(state1);
+		Place<String, String> place = Place.forState(state1);
 		assertEquals(state1, place.state());
+		assertTrue(place.tokens().isEmpty());
 	}
-	
+
 	@Test
 	void putsTokenOnPlace() {
 		State<String, String> state1 = state("State1", s -> true);
-		Place<String,String> place = Place.forState(state1);
-		Place<String,String> placeWithToken1 = place.putToken("Token1");
-		assertEquals("Token1", placeWithToken1.tokens().get(0));
+		Place<String, String> place = Place.forState(state1);
+		Place<String, String> placeWithToken1 = place.putToken("Token1");
+		assertEquals(asList("Token1"), placeWithToken1.tokens());
 	}
-	
+
 	@Test
 	void putsDifferentTokenOnPlace() {
 		State<Number, Number> state1 = state("State1", s -> true);
-		Place<Number,Number> place = Place.forState(state1);
-		Place<Number,Number> placeWithToken1 = place.putToken(5);
-		assertEquals(5, placeWithToken1.tokens().get(0));
+		Place<Number, Number> place = Place.forState(state1);
+		Place<Number, Number> placeWithToken1 = place.putToken(5);
+		assertEquals(asList(5), placeWithToken1.tokens());
 	}
-	
+
 	@Test
 	void putsTwoTokensOnPlace() {
 		State<Token, Token> state1 = state("State1", s -> true);
 		TokenType1 token1 = new TokenType1();
 		TokenType2 token2 = new TokenType2();
-		
-		Place<Token,Token> placeWithTwoTokens = Place.forState(state1).withTokens(token1, token2);
-		
+
+		Place<Token, Token> placeWithTwoTokens = Place.forState(state1).withTokens(token1, token2);
 		assertEquals(asList(token1, token2), placeWithTwoTokens.tokens());
 	}
 
@@ -47,15 +48,20 @@ class PlaceTest {
 		State<Token, Token> state1 = state("State1", s -> true);
 		TokenType1 token1 = new TokenType1();
 		TokenType2 token2 = new TokenType2();
-		
-		Place<Token,Token> placeWithOneToken = Place.forState(state1).withTokens(token1);
-		Place<Token,Token> placeWithTwoTokens = placeWithOneToken.putToken(token2);
-		
+
+		Place<Token, Token> placeWithOneToken = Place.forState(state1).withTokens(token1);
+		Place<Token, Token> placeWithTwoTokens = placeWithOneToken.putToken(token2);
+
 		assertEquals(asList(token1, token2), placeWithTwoTokens.tokens());
-		assertEquals(1, placeWithOneToken.tokens().size());
+		assertEquals(asList(token1), placeWithOneToken.tokens());
 	}
-	
-	private interface Token{};
-	private class TokenType1 implements Token{};
-	private class TokenType2 implements Token{};
+
+	private interface Token {
+	};
+
+	private class TokenType1 implements Token {
+	};
+
+	private class TokenType2 implements Token {
+	};
 }
