acdaf39d0f308e83e0f187d9974c9be2d58bd29b
Bertil Muth <bertilmuth@yahoo.de>
2022-10-12 09:17:10 +0200

    Make places general

--- act-places/src/test/java/org/requirementsascode/act/places/Places.java
+++ act-places/src/test/java/org/requirementsascode/act/places/Places.java
@@ -2,17 +2,30 @@ package org.requirementsascode.act.places;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.requirementsascode.act.statemachine.State;
 import org.requirementsascode.act.statemachine.Statemachine;
 
 public class Places<S, V> {
+	private final Statemachine<S, V> statemachine;
+
+	private Places(Statemachine<S,V> statemachine) {
+		this.statemachine = statemachine;
+	}
 
 	public static <S,V> Places<S, V> forStatemachine(Statemachine<S, V> statemachine) {
-		return new Places<>();
+		return new Places<>(statemachine);
 	}
 
 	public List<Place<S,V>> asList() {
-		return new ArrayList<>();
+		List<State<S, V>> states = statemachine.states().asList();
+		
+		List<Place<S, V>> places = states.stream()
+			.map(Place::forState)
+			.collect(Collectors.toList());
+
+		return places;
 	}
 
 }
--- act-places/src/test/java/org/requirementsascode/act/places/PlacesTest.java
+++ act-places/src/test/java/org/requirementsascode/act/places/PlacesTest.java
@@ -1,10 +1,13 @@
 package org.requirementsascode.act.places;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.requirementsascode.act.statemachine.StatemachineApi.state;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.requirementsascode.act.statemachine.State;
 import org.requirementsascode.act.statemachine.Statemachine;
 
 class PlacesTest {
@@ -19,4 +22,47 @@ class PlacesTest {
 		assertTrue(placesList.isEmpty());
 	}
 
+	@Test
+	void createsPlacesForStatemachineWithOneState() {
+		State<String,String> state1 = state("State1", s -> true);
+		Statemachine<String, String> statemachine = Statemachine.builder()
+			.states(state1)
+			.transitions()
+			.build();
+		
+		Places<String, String> places = Places.forStatemachine(statemachine);
+		List<Place<String, String>> placesList = places.asList();
+		assertEquals(1, placesList.size());
+		assertEquals(state1, placesList.get(0).state());
+	}
+	
+	@Test
+	void createsPlacesForStatemachineWithDifferentState() {
+		State<Integer,Integer> state1 = state("DifferentState", s -> true);
+		Statemachine<Integer, Integer> statemachine = Statemachine.builder()
+			.states(state1)
+			.transitions()
+			.build();
+		
+		Places<Integer, Integer> places = Places.forStatemachine(statemachine);
+		List<Place<Integer, Integer>> placesList = places.asList();
+		assertEquals(1, placesList.size());
+		assertEquals(state1, placesList.get(0).state());
+	}
+	
+	@Test
+	void createsPlacesForStatemachineWithTwoStates() {
+		State<Integer,Integer> state1 = state("State1", s -> true);
+		State<Integer,Integer> state2 = state("State2", s -> true);
+		Statemachine<Integer, Integer> statemachine = Statemachine.builder()
+			.states(state1,state2)
+			.transitions()
+			.build();
+		
+		Places<Integer, Integer> places = Places.forStatemachine(statemachine);
+		List<Place<Integer, Integer>> placesList = places.asList();
+		assertEquals(2, placesList.size());
+		assertEquals(state1, placesList.get(0).state());
+		assertEquals(state2, placesList.get(1).state());
+	}
 }
