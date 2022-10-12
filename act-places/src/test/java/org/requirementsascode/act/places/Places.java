122664900038cd4b5c0271ddd028acdbc91c5c9b
Bertil Muth <bertilmuth@yahoo.de>
2022-10-12 09:21:03 +0200

    Extract method for place creation

--- act-places/src/test/java/org/requirementsascode/act/places/Places.java
+++ act-places/src/test/java/org/requirementsascode/act/places/Places.java
@@ -1,21 +1,18 @@
 package org.requirementsascode.act.places;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.requirementsascode.act.statemachine.State;
 import org.requirementsascode.act.statemachine.Statemachine;
 
 public class Places<S, V> {
-	private final Statemachine<S, V> statemachine;
 	private final List<Place<S, V>> places;
 
 	private Places(Statemachine<S,V> statemachine) {
-		this.statemachine = statemachine;
-		List<State<S, V>> states = statemachine.states().asList();
-		places = states.stream()
-			.map(Place::forState)
-			.collect(Collectors.toList());
+		Objects.requireNonNull(statemachine, "statemachine must be non-null!");
+		this.places = createPlaces(statemachine);
 	}
 
 	public static <S,V> Places<S, V> forStatemachine(Statemachine<S, V> statemachine) {
@@ -26,4 +23,10 @@ public class Places<S, V> {
 		return places;
 	}
 
+	private List<Place<S, V>> createPlaces(Statemachine<S, V> statemachine) {
+		List<State<S, V>> states = statemachine.states().asList();
+		return states.stream()
+			.map(Place::forState)
+			.collect(Collectors.toList());
+	}
 }
