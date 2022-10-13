9367b7dc7b27b219ad5f41a484e13b250f9fe806
Bertil Muth <bertilmuth@yahoo.de>
2022-10-13 13:25:43 +0200

    Create tokens with several elements

--- act-places/src/main/java/org/requirementsascode/act/places/Tokens.java
+++ act-places/src/main/java/org/requirementsascode/act/places/Tokens.java
@@ -1,14 +1,24 @@
 package org.requirementsascode.act.places;
 
+import static java.util.Arrays.asList;
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Tokens<S, V> {
+	private final List<Token<S,V>> tokens;
 
-	public static <S,V> Tokens<S, V> of() {
-		return new Tokens<>();
+	public Tokens(List<Token<S, V>> tokens) {
+		this.tokens = requireNonNull(tokens);
+	}
+
+	@SafeVarargs
+	public static <S,V> Tokens<S, V> of(Token<S,V>... tokens) {
+		return new Tokens<>(asList(tokens));
 	}
 
 	public Stream<Token<S,V>> stream() {
-		return Stream.of();
+		return tokens.stream();
 	}
 }
--- act-places/src/test/java/org/requirementsascode/act/places/TokensTest.java
+++ act-places/src/test/java/org/requirementsascode/act/places/TokensTest.java
@@ -1,12 +1,16 @@
 package org.requirementsascode.act.places;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.requirementsascode.act.statemachine.StatemachineApi.state;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.requirementsascode.act.statemachine.State;
 
 class TokensTest {
+	private static final String TOKEN1 = "Token1";
 
 	@Test
 	void createsEmptyTokens() {
@@ -16,4 +20,15 @@ class TokensTest {
 		assertTrue(tokenList.isEmpty());
 	}
 
+	@Test
+	void createsTokensWithOneElement() {
+		State<String, String> state1 = state("State1", s -> true);
+
+		Tokens<String,String> tokens = Tokens.of(
+			Token.create(state1, TOKEN1)
+		);
+		
+		List<Token<String,String>> tokenList = tokens.stream().toList();
+		assertEquals(TOKEN1, tokenList.get(0).value());
+	}
 }
