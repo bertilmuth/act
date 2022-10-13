af71da19b8aacbf4b97a61a7f83116d310160426
Bertil Muth <bertilmuth@yahoo.de>
2022-10-13 11:38:47 +0200

    Add value to token

--- act-places/src/main/java/org/requirementsascode/act/places/Token.java
+++ act-places/src/main/java/org/requirementsascode/act/places/Token.java
@@ -6,20 +6,26 @@ import org.requirementsascode.act.statemachine.State;
 
 public class Token<S, V> {
 	private final State<S, V> state;
+	private final V value;
 
-	private Token(State<S, V> state) {
+	private Token(State<S, V> state, V value) {
 		this.state = requireNonNull(state, "state must be non-null!");
+		this.value = requireNonNull(value, "value must be non-null!");
 	}
 
-	public static <S, V> Token<S, V> inState(State<S, V> state) {
-		return new Token<>(state);
+	public static <S, V> Token<S, V> create(State<S, V> state, V value) {
+		return new Token<>(state, value);
 	}
 	
 	public Token<S, V> moveTo(State<S, V> state) {
-		return new Token<>(state);
+		return new Token<>(state, value);
 	}
 
 	public State<S, V> state() {
 		return state;
+	}
+	
+	public V value() {
+		return value;
 	}
 }
--- act-places/src/test/java/org/requirementsascode/act/places/TokenTest.java
+++ act-places/src/test/java/org/requirementsascode/act/places/TokenTest.java
@@ -7,22 +7,26 @@ import org.junit.jupiter.api.Test;
 import org.requirementsascode.act.statemachine.State;
 
 class TokenTest {
+	private static final String TOKEN1 = "Token1";
+
 	@Test
 	void createsTokenInState() {
 		State<String, String> state1 = state("State1", s -> true);
-		Token<String, String> token = Token.inState(state1);
+		Token<String, String> token = Token.create(state1, TOKEN1);
 		assertEquals(state1, token.state());
+		assertEquals(TOKEN1, token.value());
 	}
 
 	@Test
 	void movesTokenToAnotherState() {
 		State<String, String> state1 = state("State1", s -> true);
 		State<String, String> state2 = state("State2", s -> true);
-		Token<String, String> tokenInState1 = Token.inState(state1);
+		Token<String, String> tokenInState1 = Token.create(state1, TOKEN1);
 		Token<String, String> tokenInState2 = tokenInState1.moveTo(state2);
 		
 		assertEquals(state1, tokenInState1.state());
 		assertEquals(state2, tokenInState2.state());
+		assertEquals(TOKEN1, tokenInState2.value());
 	}
 }		
 
