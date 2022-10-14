6d9f58a9483b46789650a48c37be96fdc9188b19
Bertil Muth <bertilmuth@yahoo.de>
2022-10-14 23:28:29 +0200

    Refactor TransmitTokens

--- act-token/src/main/java/org/requirementsascode/act/token/TransmitTokens.java
+++ act-token/src/main/java/org/requirementsascode/act/token/TransmitTokens.java
@@ -4,29 +4,19 @@ import static org.requirementsascode.act.core.Data.data;
 import static org.requirementsascode.act.statemachine.StatemachineApi.transition;
 import static org.requirementsascode.act.token.Token.token;
 
-import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
 import org.requirementsascode.act.statemachine.State;
 import org.requirementsascode.act.statemachine.Transition;
 
-public class TransmitTokens<V> implements Behavior<Tokens<V>, V, V>{
-	private final State<?, V> sourceState;
-	private final State<?, V> targetState;
-	
-	public TransmitTokens(State<?, V> sourceState, State<?, V> targetState) {
-		this.sourceState = sourceState;
-		this.targetState = targetState;
-	}
-
+public class TransmitTokens<V>{
 	public static <V> Transition<Tokens<V>,V> transmitTokens(State<Tokens<V>, V> sourceState, State<Tokens<V>, V> targetState) {
-		return transition(sourceState, targetState, new TransmitTokens<>(sourceState, targetState));
+		return transition(sourceState, targetState, d -> transmit(d, sourceState, targetState));
 	}
 
-	@Override
-	public Data<Tokens<V>, V> actOn(Data<Tokens<V>, V> before) {
-		assert(before.value().isPresent());
-		Tokens<V> tokensBefore = before.state();
-		V beforeValue = before.value().get();
+	private static <V> Data<Tokens<V>, V> transmit(Data<Tokens<V>, V> d, State<Tokens<V>, V> sourceState, State<Tokens<V>, V> targetState) {
+		assert(d.value().isPresent());
+		Tokens<V> tokensBefore = d.state();
+		V beforeValue = d.value().get();
 		Tokens<V> tokensAfter = tokensBefore.moveToken(token(beforeValue, sourceState), targetState);
 		return data(tokensAfter, beforeValue);
 	}
