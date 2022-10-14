f2dc4d813801b5f9513d16c99217de5faea73841
Bertil Muth <bertilmuth@yahoo.de>
2022-10-14 18:20:47 +0200

    Refactor

--- act-token/src/test/java/org/requirementsascode/act/token/TokenFlowTest.java
+++ act-token/src/test/java/org/requirementsascode/act/token/TokenFlowTest.java
@@ -10,6 +10,7 @@ import static org.requirementsascode.act.token.Token.token;
 import static org.requirementsascode.act.token.Tokens.tokens;
 
 import java.util.Objects;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 import org.requirementsascode.act.core.Data;
@@ -23,10 +24,10 @@ class TokenFlowTest {
 
 	@Test
 	void test() {
-		State<Tokens<Trigger>, Trigger> state1 = state(STATE1, tokens -> tokens.inState(STATE1).count() != 0, 
+		State<Tokens<Trigger>, Trigger> state1 = state(STATE1, tokens -> isAnyTokenInState(tokens, STATE1), 
 			d -> publishToken(STATE1, d));
 		
-		State<Tokens<Trigger>, Trigger> state2 = state(STATE2, tokens -> tokens.inState(STATE2).count() != 0,
+		State<Tokens<Trigger>, Trigger> state2 = state(STATE2, tokens -> isAnyTokenInState(tokens, STATE2), 
 			d -> publishToken(STATE2, d));
 		
 		Statemachine<Tokens<Trigger>, Trigger> statemachine =
@@ -42,13 +43,19 @@ class TokenFlowTest {
 				)
 				.build();
 		
+		Value value1 = new Value(VALUE1);
+		
 		Tokens<Trigger> tokens = tokens(
-				token(new Value(VALUE1), state1)
+				token(value1, state1)
 		);
 		
 		Data<Tokens<Trigger>, Trigger> dataAfter = statemachine.actOn(data(tokens, new Tick()));
 		assertFalse(dataAfter.state().inState(STATE1).findAny().isPresent());
-		assertEquals(token(new Value(VALUE1), state2), dataAfter.state().inState(STATE2).findFirst().get());
+		assertEquals(token(value1, state2), dataAfter.state().inState(STATE2).findFirst().get());
+	}
+
+	private <V> boolean isAnyTokenInState(Tokens<Trigger> tokens, String stateName) {
+		return tokens.inState(stateName).count() != 0;
 	}
 
 	private <V> Data<Tokens<V>, V> publishToken(String stateName, Data<Tokens<V>, V> data) {
