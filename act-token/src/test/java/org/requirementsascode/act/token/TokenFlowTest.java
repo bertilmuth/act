d633e7633f404dfa14119daf772405d7caae63aa
Bertil Muth <bertilmuth@yahoo.de>
2022-10-14 16:54:36 +0200

    Use Value as Token type

--- act-token/src/test/java/org/requirementsascode/act/token/TokenFlowTest.java
+++ act-token/src/test/java/org/requirementsascode/act/token/TokenFlowTest.java
@@ -5,7 +5,6 @@ import static org.requirementsascode.act.statemachine.StatemachineApi.state;
 import static org.requirementsascode.act.statemachine.StatemachineApi.transition;
 
 import org.junit.jupiter.api.Test;
-import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
 import org.requirementsascode.act.statemachine.State;
 import org.requirementsascode.act.statemachine.Statemachine;
@@ -16,35 +15,40 @@ class TokenFlowTest {
 
 	@Test
 	void test() {
-		State<Tokens<String>, String> state1 = state(STATE1, tokens -> tokens.inState(STATE1).count() != 0, 
-			this::publishToken);
+		State<Tokens<Value>, Value> state1 = state(STATE1, tokens -> tokens.inState(STATE1).count() != 0, 
+			d -> publishToken(STATE1, d));
 		
-		State<Tokens<String>, String> state2 = state(STATE2, tokens -> tokens.inState(STATE2).count() != 0,
-			this::publishToken);
+		State<Tokens<Value>, Value> state2 = state(STATE2, tokens -> tokens.inState(STATE2).count() != 0,
+			d -> publishToken(STATE2, d));
 		
-		Statemachine<Tokens<String>, String> statemachine =
+		Statemachine<Tokens<Value>, Value> statemachine =
 			Statemachine.builder()
 				.states(state1, state2)
 				.transitions(
 					transition(state1, state2, 
 						consumeWith((tokens,value) -> {
-							Token<String> token = Token.token(value, state1);
-							Tokens<String> newTokens = tokens.moveToken(token, state2);
+							Token<Value> token = Token.token(value, state1);
+							Tokens<Value> newTokens = tokens.moveToken(token, state2);
 							return newTokens;
 						}))
 				)
 				.build();
 		
-		Tokens<String> tokens = Tokens.tokens(
-				Token.token("Token1", state1)
+		Tokens<Value> tokens = Tokens.tokens(
+				Token.token(new Value(), state1)
 		);
+		
+		statemachine.actOn(Data.data(tokens, new Tick()));
 	}
 
-	private Data<Tokens<String>, String> publishToken(Data<Tokens<String>, String> data) {
-			String firstTokenValue = data.state().inState(STATE1)
+	private <V> Data<Tokens<V>, V> publishToken(String stateName, Data<Tokens<V>, V> data) {
+			V firstTokenValue = data.state().inState(stateName)
 				.findFirst()
 				.map(t -> t.value())
 				.orElse(null);
 			return Data.data(data.state(), firstTokenValue);
 	}
+	
+	private static class Value{};
+	private static class Tick extends Value{};
 }
