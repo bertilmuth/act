0c07f18dc53cb7d10416a1df69eabf50a3a3a19c
Bertil Muth <bertilmuth@yahoo.de>
2022-10-15 13:23:10 +0200

    Statically import WorkflowState

--- act-token/src/main/java/org/requirementsascode/act/token/Action.java
+++ act-token/src/main/java/org/requirementsascode/act/token/Action.java
@@ -1,14 +1,20 @@
 package org.requirementsascode.act.token;
 
+import static java.util.Objects.requireNonNull;
+
 import static org.requirementsascode.act.core.Data.data;
 import static org.requirementsascode.act.statemachine.StatemachineApi.state;
 
 import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
 import org.requirementsascode.act.statemachine.State;
+import static org.requirementsascode.act.token.WorkflowState.workflowState;
 
 public class Action {
 	public static <V> State<WorkflowState<V>, V> action(String stateName, Behavior<WorkflowState<V>, V, V> actionBehavior) {
+		requireNonNull(stateName, "stateName must be non-null!");
+		requireNonNull(actionBehavior, "actionBehavior must be non-null!");
+
 		return state(stateName, workflowState -> isAnyTokenInState(workflowState, stateName), 
 			d -> act(stateName, d, actionBehavior));
 	}
@@ -23,6 +29,6 @@ public class Action {
 			V firstTokenValue = tokens.firstTokenInState(stateName)
 				.map(t -> t.value())
 				.orElse(null);
-			return data(WorkflowState.workflowState(tokens), firstTokenValue);
+			return data(workflowState(tokens), firstTokenValue);
 	}
 }
