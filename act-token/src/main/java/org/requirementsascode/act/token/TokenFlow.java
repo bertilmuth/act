8167b190cf93a0e6b06e4a63baf20dc90b56fe1c
Bertil Muth <bertilmuth@yahoo.de>
2022-10-15 13:18:51 +0200

    Statically import WorkflowState

--- act-token/src/main/java/org/requirementsascode/act/token/TokenFlow.java
+++ act-token/src/main/java/org/requirementsascode/act/token/TokenFlow.java
@@ -1,8 +1,10 @@
 package org.requirementsascode.act.token;
 
+import static java.util.Objects.requireNonNull;
 import static org.requirementsascode.act.core.Data.data;
 import static org.requirementsascode.act.statemachine.StatemachineApi.transition;
 import static org.requirementsascode.act.token.Token.token;
+import static org.requirementsascode.act.token.WorkflowState.workflowState;
 
 import org.requirementsascode.act.core.Data;
 import org.requirementsascode.act.statemachine.Flow;
@@ -15,8 +17,8 @@ public class TokenFlow<V> implements Flow<WorkflowState<V>, V>{
 	private final State<WorkflowState<V>, V> targetState;
 
 	private TokenFlow(State<WorkflowState<V>, V> sourceState, State<WorkflowState<V>, V> targetState) {
-		this.sourceState = sourceState;
-		this.targetState = targetState;
+		this.sourceState = requireNonNull(sourceState, "sourceState must be non-null!");
+		this.targetState = requireNonNull(targetState, "targetState must be non-null!");
 	}
 	
 	public static <V> TokenFlow<V> tokenFlow(State<WorkflowState<V>, V> sourceState, State<WorkflowState<V>, V> targetState) {		
@@ -33,6 +35,6 @@ public class TokenFlow<V> implements Flow<WorkflowState<V>, V>{
 		Tokens<V> tokensBefore = d.state().tokens();
 		V beforeValue = d.value().get();
 		Tokens<V> tokensAfter = tokensBefore.moveToken(token(beforeValue, sourceState), targetState);
-		return data(WorkflowState.workflowState(tokensAfter), beforeValue);
+		return data(workflowState(tokensAfter), beforeValue);
 	}
 }
