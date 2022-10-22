ada4338e221c420ae4cb4c1a3fa3b024de8f08c3
Bertil Muth <bertilmuth@yahoo.de>
2022-10-22 14:39:14 +0200

    Refactor

--- act-token/src/main/java/org/requirementsascode/act/token/function/Atomic.java
+++ act-token/src/main/java/org/requirementsascode/act/token/function/Atomic.java
@@ -18,32 +18,32 @@ import org.requirementsascode.act.token.Workflow;
 public class Atomic<T extends ActionData, U extends ActionData> implements ActionBehavior {
 	private final SystemFunction<T, U> systemFunction;
 
-	private Atomic(SystemFunction<T,U> systemFunction) {
+	private Atomic(SystemFunction<T, U> systemFunction) {
 		this.systemFunction = systemFunction;
 	}
 
-	public static <T extends ActionData, U extends ActionData> Atomic<T,U> atomic(Class<T> inputClass, BiFunction<Workflow, T, U> function) {
+	public static <T extends ActionData, U extends ActionData> Atomic<T, U> atomic(Class<T> inputClass,
+			BiFunction<Workflow, T, U> function) {
 		return new Atomic<>(systemFunction(inputClass, function));
 	}
 
 	@Override
 	public Behavior<Workflow, Token, Token> asBehavior(Action owningAction) {
-		return inCase(this::triggersAtomicSystemFunction, d -> triggerAtomicSystemFunction(owningAction, d));
+		return inCase(this::triggersAtomicSystemBehavior, d -> atomicSystemFunction(owningAction, d));
 	}
 
-	private boolean triggersAtomicSystemFunction(Data<Workflow, Token> inputData) {
-		Optional<Token> token = Token.from(inputData);
-		return token.map(t -> triggersAtomicSystemFunction(t)).orElse(false);
+	private boolean triggersAtomicSystemBehavior(Data<Workflow, Token> inputData) {
+		return Token.from(inputData).map(t -> triggersAtomicSystemFunction(t)).orElse(false);
 	}
 
-	private Data<Workflow, Token> triggerAtomicSystemFunction(Action owningAction, Data<Workflow, Token> inputData) {
+	private Data<Workflow, Token> atomicSystemFunction(Action owningAction, Data<Workflow, Token> inputData) {
 		Workflow workflow = Workflow.from(inputData);
 		Token tokenInAction = workflow.tokens().firstTokenIn(owningAction.name()).get();
 		Data<Workflow, Token> inputDataWithTokenInAction = data(workflow, tokenInAction);
-		Data<Workflow,Token> outputData = systemFunction.executeFunction(inputDataWithTokenInAction);
+		Data<Workflow, Token> outputData = systemFunction.executeFunction(inputDataWithTokenInAction);
 		return outputData;
 	}
-	
+
 	private boolean triggersAtomicSystemFunction(Token token) {
 		return token.actionData() instanceof AtomicSystemFunction;
 	}
