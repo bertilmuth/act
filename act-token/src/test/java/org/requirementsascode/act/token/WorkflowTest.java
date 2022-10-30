9281657df15a63588efa994ef1836c43d2213105
Bertil Muth <bertilmuth@yahoo.de>
2022-10-30 16:21:32 +0100

    Add test

--- act-token/src/test/java/org/requirementsascode/act/token/WorkflowTest.java
+++ act-token/src/test/java/org/requirementsascode/act/token/WorkflowTest.java
@@ -1,13 +1,19 @@
 package org.requirementsascode.act.token;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.requirementsascode.act.token.Action.action;
+import static org.requirementsascode.act.token.Token.token;
+import static org.requirementsascode.act.token.function.Atomic.atomic;
 
 import org.junit.jupiter.api.Test;
 import org.requirementsascode.act.token.Workflow.AfterStep;
 
 class WorkflowTest {
-
+	private static final String START_WORKFLOW = "";
+	private static final String ACTION1 = "Action1";
+	
 	@Test
 	void runningEmptyWorkflowDoesNothing() {
 		Workflow workflow = Workflow.builder()
@@ -20,5 +26,27 @@ class WorkflowTest {
 		
 		assertTrue(workflowStarted.actionOutput().isEmpty());
 		assertFalse(workflowStarted.tokens().isAnyTokenIn(""));
+	}
+	
+	@Test
+	void runsSingleAction() {
+		Action action1 = action(ACTION1, atomic(StringValue.class, this::action1Performed));
+		
+		Workflow workflow = Workflow.builder()
+			.actions(action1)
+			.tokenFlows()
+			.initialActions(action1)
+			.build();
+		
+		StringValue startWorkflow = new StringValue(START_WORKFLOW);
+		AfterStep workflowStarted = workflow.nextStep(startWorkflow);
+	
+		Tokens tokensAtStart = workflowStarted.tokens();
+		assertEquals(token(action1, startWorkflow), tokensAtStart.firstTokenIn(ACTION1).get());
+
+	}
+	
+	private StringValue action1Performed(Workflow workflow, StringValue input) {
+		return new StringValue(ACTION1);
 	}
 }
