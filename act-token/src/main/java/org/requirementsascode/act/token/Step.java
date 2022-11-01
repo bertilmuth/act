dcd878f042ce49b9bbb0024e2c08b9e31f7b0a00
Bertil Muth <bertilmuth@yahoo.de>
2022-11-01 09:22:17 +0100

    Make SystemFunction a private class

--- act-token/src/main/java/org/requirementsascode/act/token/Step.java
+++ act-token/src/main/java/org/requirementsascode/act/token/Step.java
@@ -1,9 +1,9 @@
 package org.requirementsascode.act.token;
 
+import static java.util.Objects.requireNonNull;
 import static org.requirementsascode.act.core.Data.data;
 import static org.requirementsascode.act.core.InCase.inCase;
 import static org.requirementsascode.act.statemachine.StatemachineApi.when;
-import static org.requirementsascode.act.token.SystemFunction.systemFunction;
 import static org.requirementsascode.act.token.Token.token;
 
 import java.util.function.BiFunction;
@@ -12,16 +12,18 @@ import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
 
 public class Step<T extends ActionData, U extends ActionData> implements ActionBehavior {
-	private final SystemFunction<T, U> systemFunction;
+	private final SystemFunction systemFunction;
 	public static final StepTrigger stepTrigger = new StepTrigger();
 
-	private Step(SystemFunction<T, U> systemFunction) {
-		this.systemFunction = systemFunction;
+	private Step(Class<T> inputClass, BiFunction<Workflow, T, U> function) {
+		requireNonNull(inputClass, "inputClass must be non-null!");
+		requireNonNull(function, "function must be non-null!");
+		this.systemFunction = new SystemFunction(inputClass, function);
 	}
 
 	public static <T extends ActionData, U extends ActionData> Step<T, U> step(Class<T> inputClass,
 			BiFunction<Workflow, T, U> function) {
-		return new Step<>(systemFunction(inputClass, function));
+		return new Step<>(inputClass, function);
 	}
 
 	@Override
@@ -46,56 +48,52 @@ public class Step<T extends ActionData, U extends ActionData> implements ActionB
 		private StepTrigger() {
 		};
 	}
-}
+	
+	private class SystemFunction implements ActionBehavior {
+		private final Behavior<Workflow, ActionData, ActionData> functionOnActionData;
 
-class SystemFunction<T extends ActionData, U extends ActionData> implements ActionBehavior {
-	private final Behavior<Workflow, ActionData, ActionData> functionOnActionData;
+		private SystemFunction(Class<T> inputClass, BiFunction<Workflow, T, U> functionOnActionData) {
+			Behavior<Workflow, T, U> behavior = d -> apply(functionOnActionData, d);
+			this.functionOnActionData = when(inputClass, behavior);
+		}
 
-	private SystemFunction(Class<T> inputClass, BiFunction<Workflow, T, U> functionOnActionData) {
-		Behavior<Workflow, T, U> behavior = d -> apply(functionOnActionData, d);
-		this.functionOnActionData = when(inputClass, behavior);
-	}
+		@Override
+		public Behavior<Workflow, Token, Token> asBehavior(Action owningAction) {
+			return this::executeFunction;
+		}
 
-	public static <T extends ActionData, U extends ActionData> SystemFunction<T, U> systemFunction(Class<T> inputClass,
-			BiFunction<Workflow, T, U> function) {
-		return new SystemFunction<>(inputClass, function);
-	}
+		private Data<Workflow, Token> executeFunction(Data<Workflow, Token> inputData) {
+			Data<Workflow, ActionData> inputActionData = unboxActionData(inputData);
+			Data<Workflow, ActionData> outputActionData = functionOnActionData.actOn(inputActionData);
+			
+			Token inputToken = tokenFrom(inputData);
+			Token outputToken = updateActionData(inputToken, outputActionData);
+			
+			return Workflow.from(inputData).replaceToken(inputToken, outputToken);
+		}
 
-	@Override
-	public Behavior<Workflow, Token, Token> asBehavior(Action owningAction) {
-		return this::executeFunction;
-	}
+		private Data<Workflow, ActionData> unboxActionData(Data<Workflow, Token> inputData) {
+			return data(Workflow.from(inputData), actionDataFrom(inputData));
+		}
 
-	private Data<Workflow, Token> executeFunction(Data<Workflow, Token> inputData) {
-		Data<Workflow, ActionData> inputActionData = unboxActionData(inputData);
-		Data<Workflow, ActionData> outputActionData = functionOnActionData.actOn(inputActionData);
-		
-		Token inputToken = tokenFrom(inputData);
-		Token outputToken = updateActionData(inputToken, outputActionData);
-		
-		return Workflow.from(inputData).replaceToken(inputToken, outputToken);
-	}
+		private Token tokenFrom(Data<Workflow, Token> inputData) {
+			return Token.from(inputData).orElseThrow(() -> new IllegalArgumentException("No token present!"));
+		}
 
-	private Data<Workflow, ActionData> unboxActionData(Data<Workflow, Token> inputData) {
-		return data(Workflow.from(inputData), actionDataFrom(inputData));
-	}
+		private Token updateActionData(Token token, Data<Workflow, ActionData> data) {
+			return token(token.node(), data.value().orElse(null));
+		}
 
-	private Token tokenFrom(Data<Workflow, Token> inputData) {
-		return Token.from(inputData).orElseThrow(() -> new IllegalArgumentException("No token present!"));
-	}
+		private ActionData actionDataFrom(Data<Workflow, Token> inputData) {
+			return Token.from(inputData).flatMap(Token::actionData).orElse(null);
+		}
 
-	private Token updateActionData(Token token, Data<Workflow, ActionData> data) {
-		return token(token.node(), data.value().orElse(null));
-	}
-
-	private ActionData actionDataFrom(Data<Workflow, Token> inputData) {
-		return Token.from(inputData).flatMap(Token::actionData).orElse(null);
-	}
-
-	private Data<Workflow, U> apply(BiFunction<Workflow, T, U> function, Data<Workflow, T> input) {
-		Workflow workflow = Workflow.from(input);
-		T inputActionData = input.value().orElse(null);
-		U outputActionData = function.apply(workflow, inputActionData);
-		return data(workflow, outputActionData);
+		private Data<Workflow, U> apply(BiFunction<Workflow, T, U> function, Data<Workflow, T> input) {
+			Workflow workflow = Workflow.from(input);
+			T inputActionData = input.value().orElse(null);
+			U outputActionData = function.apply(workflow, inputActionData);
+			return data(workflow, outputActionData);
+		}
 	}
 }
+
