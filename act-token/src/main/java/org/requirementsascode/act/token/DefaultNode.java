6b27baf0a55e67a564b31fdb545b658b7318842b
Bertil Muth <bertilmuth@yahoo.de>
2022-11-01 09:51:02 +0100

    Revert "Move DefaultNode to InitialAction"
    This reverts commit 4c6c8a6f0fea15e5c460ee2d1d005c92cb98c921.

---
+++ act-token/src/main/java/org/requirementsascode/act/token/DefaultNode.java
@@ -0,0 +1,29 @@
+package org.requirementsascode.act.token;
+
+import static java.util.Objects.requireNonNull;
+
+import org.requirementsascode.act.statemachine.State;
+import org.requirementsascode.act.statemachine.Statemachine;
+
+class DefaultNode implements Node {
+	private final State<Workflow, Token> defaultState;
+
+	private DefaultNode(Statemachine<Workflow, Token> statemachine) {
+		requireNonNull(statemachine, "statemachine must be non-null!");
+		this.defaultState = statemachine.defaultState();
+	}
+
+	public static DefaultNode defaultNode(Statemachine<Workflow, Token> statemachine) {
+		return new DefaultNode(statemachine);
+	}
+
+	@Override
+	public String name() {
+		return defaultState.name();
+	}
+
+	@Override
+	public State<Workflow, Token> asState() {
+		return defaultState;
+	}
+}
