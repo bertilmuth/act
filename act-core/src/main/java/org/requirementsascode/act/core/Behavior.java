f566b8497ac44c42260223bece681cf9f4eac165
Bertil Muth <bertilmuth@yahoo.de>
2022-11-03 15:12:15 +0100

    Improve formatting

--- act-core/src/main/java/org/requirementsascode/act/core/Behavior.java
+++ act-core/src/main/java/org/requirementsascode/act/core/Behavior.java
@@ -5,23 +5,23 @@ import static org.requirementsascode.act.core.Change.change;
 import java.util.Objects;
 
 public interface Behavior<S, V1, V2> {
-	Data<S,V2> actOn(Data<S,V1> before);
+	Data<S, V2> actOn(Data<S, V1> before);
 
-	default <V3> Behavior<S, V1, V3> andThen(Behavior<S, V2, V3> nextBehavior){
-    Objects.requireNonNull(nextBehavior, "nextBehavior must be non-null!");    
-    return before -> nextBehavior.actOn(actOn(before));
+	default <V3> Behavior<S, V1, V3> andThen(Behavior<S, V2, V3> nextBehavior) {
+		Objects.requireNonNull(nextBehavior, "nextBehavior must be non-null!");
+		return before -> nextBehavior.actOn(actOn(before));
 	}
-	
-	default Behavior<S, V1, V2> andHandleChangeWith(HandleChange<S, V1, V2> changeHandler){
-    Objects.requireNonNull(changeHandler, "changeHandler must be non-null!");    
+
+	default Behavior<S, V1, V2> andHandleChangeWith(HandleChange<S, V1, V2> changeHandler) {
+		Objects.requireNonNull(changeHandler, "changeHandler must be non-null!");
 		return before -> {
 			Data<S, V2> after = actOn(before);
 			Change<S, V1, V2> change = change(before, after);
 			return changeHandler.handleChange(change);
 		};
 	}
-	
-	static <S,V1> Behavior<S, V1, V1> identity() {
-    return d -> d;
-  }
+
+	static <S, V1> Behavior<S, V1, V1> identity() {
+		return d -> d;
+	}
 }
