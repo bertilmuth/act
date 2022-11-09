474cc8532da996ac367ca296393a34d417e62a42
Bertil Muth <bertilmuth@yahoo.de>
2022-11-09 14:47:20 +0100

    Make test pass

--- act-workflow/src/test/java/org/requirementsascode/act/workflow/TokensDifference.java
+++ act-workflow/src/test/java/org/requirementsascode/act/workflow/TokensDifference.java
@@ -1,12 +1,24 @@
 package org.requirementsascode.act.workflow;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class TokensDifference {
 
 	public static Tokens tokensAdded(Tokens tokensBefore, Tokens tokensAfter) {
-		List<Token> tokenList = tokensAfter.stream().toList();
-		return new Tokens(tokenList);
+		List<Token> tokens = plus(tokensBefore, tokensAfter);
+		tokens.removeAll(tokensBefore.stream().toList());
+
+		return new Tokens(tokens);
+	}
+
+	private static List<Token> plus(Tokens tokensBefore, Tokens tokensAfter) {
+		List<Token> tokensBeforeList = tokensBefore.stream().toList();
+		List<Token> tokensAfterList = tokensAfter.stream().toList();
+		List<Token> tokensList = new ArrayList<>();
+		tokensList.addAll(tokensBeforeList);
+		tokensList.addAll(tokensAfterList);
+		return tokensList;
 	}
 
 }
