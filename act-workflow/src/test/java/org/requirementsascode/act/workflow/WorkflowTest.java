e11c5b89d8dbd5373ddfee6273d5948a4ec9b0d6
Bertil Muth <bertilmuth@yahoo.de>
2022-11-11 13:47:17 +0100

    Adapt test

--- act-workflow/src/test/java/org/requirementsascode/act/workflow/WorkflowTest.java
+++ act-workflow/src/test/java/org/requirementsascode/act/workflow/WorkflowTest.java
@@ -141,10 +141,9 @@ class WorkflowTest {
 		
 		WorkflowState afterAction1State = workflow.start(str(START_WORKFLOW)).state();	
 		WorkflowState afterForkState = workflow.nextStep(afterAction1State).state();
-		WorkflowState afterJoinState = workflow.nextStep(afterForkState).state();
 
-		assertEquals(token(action2a, str(ACTION2A)), afterJoinState.tokens().firstTokenIn(ACTION3).get());
-		assertEquals(2, tokensList(afterJoinState).size());
+		assertEquals(token(action3, str(ACTION2A)), afterForkState.tokens().firstTokenIn(ACTION3).get());
+		assertEquals(2, tokensList(afterForkState).size());
 	}
 	
 	@Test
