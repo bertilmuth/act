e0352ade3f6a393f630593f27f064dc318c34bfb
Bertil Muth <bertilmuth@yahoo.de>
2022-11-27 13:55:00 +0100

    Make StoreToken accept actionData

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/trigger/StoreToken.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/trigger/StoreToken.java
@@ -6,14 +6,14 @@ import org.requirementsascode.act.workflow.Token;
 import org.requirementsascode.act.workflow.WorkflowState;
 
 public class StoreToken implements ActionData {
-	private final Token token;
+	private final ActionData actionData;
 
-	public StoreToken(Token token){
-		this.token = token;
+	public StoreToken(ActionData actionData){
+		this.actionData = actionData;
 	};
 	
-	public Token token() {
-		return token;
+	public ActionData actionData() {
+		return actionData;
 	}
 	
 	public static boolean isContained(Data<WorkflowState, Token> inputData) {
