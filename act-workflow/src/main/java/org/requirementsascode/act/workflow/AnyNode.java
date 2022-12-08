20022ad8b123a7591fa54f55dbc0d410b4d95133
Bertil Muth <bertilmuth@yahoo.de>
2022-12-08 13:21:30 +0100

    Simplify AnyNode

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/AnyNode.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/AnyNode.java
@@ -1,12 +1,9 @@
 package org.requirementsascode.act.workflow;
 
-import static org.requirementsascode.act.core.InCase.inCase;
-import static org.requirementsascode.act.statemachine.StatemachineApi.anyState;
 import static org.requirementsascode.act.statemachine.StatemachineApi.state;
 
-import org.requirementsascode.act.core.Behavior;
+import org.requirementsascode.act.core.NoOp;
 import org.requirementsascode.act.statemachine.State;
-import org.requirementsascode.act.workflow.trigger.AddToken;
 
 class AnyNode implements Node {
 	AnyNode() {
@@ -14,21 +11,12 @@ class AnyNode implements Node {
 
 	@Override
 	public String name() {
-		return anyState().name();
+		return "Any Node";
 	}
 
 	@Override
 	public State<WorkflowState, Token> asState() {
-		State<WorkflowState, Token> state = state("Any Node", s -> true, anyNodeBehavior());
+		State<WorkflowState, Token> state = state(name(), s -> true, new NoOp<>());
 		return state;
-	}
-	
-	private Behavior<WorkflowState, Token, Token> anyNodeBehavior() {
-		Behavior<WorkflowState, Token, Token> behavior = 
-			inCase(AddToken::isContained, d -> {
-				Token tokenToAdd = Token.from(d).actionData().map(t -> (AddToken)t).map(AddToken::token).orElse(null);
-				return d.state().addToken(tokenToAdd);
-			});
-		return behavior;
 	}
 }
