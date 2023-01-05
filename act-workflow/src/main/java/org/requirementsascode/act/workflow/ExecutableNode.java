994a738b5ae736903e5f9bd7657c64623e97c2a9
Bertil Muth <bertilmuth@yahoo.de>
2023-01-05 08:58:06 +0100

    Simplify ExecutableNode

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/ExecutableNode.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/ExecutableNode.java
@@ -1,11 +1,7 @@
 package org.requirementsascode.act.workflow;
 
 import static java.util.Objects.requireNonNull;
-import static org.requirementsascode.act.statemachine.StatemachineApi.data;
 import static org.requirementsascode.act.statemachine.StatemachineApi.state;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
@@ -38,26 +34,11 @@ public class ExecutableNode implements Node {
 	}
 
 	private Data<WorkflowState, Token> consumeToken(Data<WorkflowState, Token> inputData) {
-		WorkflowState state = inputData.state();
-		List<Token> tokensInNode = state.tokensIn(this).collect(Collectors.toList());
-		
-		Data<WorkflowState, Token> result = data(state, null);
-		for (Token tokenInNode : tokensInNode) {
-			result = executableBehavior.actOn(data(result.state(), tokenInNode));
-			if (hasExecuted(result)) {
-				break;
-			}
-		}
-
-		return result;
-	}
-
-	private boolean hasExecuted(Data<WorkflowState, Token> result) {
-		return result.value().map(t -> t.actionData().isPresent()).orElse(false);
+		return executableBehavior.actOn(inputData);
 	}
 
 	@Override
 	public String toString() {
-		return "Action[" + name + "]";
+		return "ExecutableNode[" + name + "]";
 	}
 }
