c5c7179ac0d8f72b504130e7bdfe16cd095b797c
Bertil Muth <bertilmuth@yahoo.de>
2023-01-07 09:22:34 +0100

    Remove unnecessary imports

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/Tokens.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/Tokens.java
@@ -3,10 +3,8 @@ package org.requirementsascode.act.workflow;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
