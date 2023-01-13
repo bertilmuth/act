3fe52c832ccc2ab222e16a71b53f7483c632fb0d
Bertil Muth <bertilmuth@yahoo.de>
2023-01-13 18:04:19 +0100

    Refactor WorkflowApi

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/WorkflowApi.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/WorkflowApi.java
@@ -16,7 +16,7 @@ public class WorkflowApi {
 	}
 	
 	public static <T extends ActionData> DataFlow<T,T> dataFlow(Port<T> inputPort, Port<T> outputPort) {		
-		return new DataFlow<>(inputPort, outputPort, (s,ad) -> ad);
+		return dataFlow(inputPort, outputPort, (s,ad) -> ad);
 	}
 	
 	static <T extends ActionData, U extends ActionData> DataFlow<T,U> dataFlow(Port<T> inputPort, Port<U> outputPort, BiFunction<WorkflowState, T, U> actionFunction) {		
