f5c18ef381c5a251892cf9a97f3d44c8909933fc
Bertil Muth <bertilmuth@yahoo.de>
2023-01-14 23:12:40 +0100

    Extract TokenMergeStrategy

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/Workflow.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/Workflow.java
@@ -5,14 +5,10 @@ import static org.requirementsascode.act.statemachine.StatemachineApi.data;
 import static org.requirementsascode.act.workflow.WorkflowApi.token;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
-import org.requirementsascode.act.core.merge.MergeStrategy;
 import org.requirementsascode.act.statemachine.State;
 import org.requirementsascode.act.statemachine.Statemachine;
 import org.requirementsascode.act.statemachine.Transitionable;
@@ -82,44 +78,13 @@ public class Workflow implements Behavior<WorkflowState, Token, Token>{
 
 		Statemachine<WorkflowState, Token> statemachine = 
 			Statemachine.builder()
-				.mergeStrategy(new TokenMergeStrategy())
+				.mergeStrategy(new TokenMergeStrategy(this))
 				.states(allStates)
 				.transitions(
 					transitionables
 				)
 				.build();
 		return statemachine;
-	}
-	
-	private class TokenMergeStrategy implements MergeStrategy<WorkflowState, Token>{
-		@Override
-		public Data<WorkflowState, Token> merge(Data<WorkflowState, Token> dataBefore, List<Data<WorkflowState, Token>> datasAfter) {
-			Tokens mergedTokens = mergeTokens(datasAfter);
-			WorkflowState state = new WorkflowState(Workflow.this, mergedTokens);
-			return data(state, null);
-		}
-
-		private Tokens mergeTokens(List<Data<WorkflowState, Token>> datasAfter) {	
-			Map<Port<?>, List<Token>> mergedTokenMap = datasAfter.stream()
-				.map(Data::state).map(WorkflowState::tokens)
-				.flatMap(tkns -> tkns.asMap().entrySet().stream())
-			    .collect(Collectors.toMap(
-			        Map.Entry::getKey,
-			        Map.Entry::getValue,
-			        (v1, v2) -> { 
-			        	List<Token> mergedList = Stream.concat(v1.stream(), v2.stream())
-			        		.collect(Collectors.toList());
-			        	return mergedList; 
-			        })
-			    );
-			
-			// Remove all elements that don't have actionData set
-			mergedTokenMap.replaceAll((key, value) -> value.stream()
-				.filter(t -> t.actionData().isPresent())
-			    .collect(Collectors.toList()));
-			
-			return new Tokens(mergedTokenMap);
-		}
 	}
 }
 
