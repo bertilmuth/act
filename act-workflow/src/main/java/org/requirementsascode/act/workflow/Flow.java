aa911a4edf8aea7e6a3ca728cacfd1195f71e711
Bertil Muth <bertilmuth@yahoo.de>
2023-01-16 17:19:46 +0100

    Work on Flow

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/Flow.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/Flow.java
@@ -4,8 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static org.requirementsascode.act.core.InCase.inCase;
 import static org.requirementsascode.act.statemachine.StatemachineApi.data;
 import static org.requirementsascode.act.statemachine.StatemachineApi.transition;
+import static org.requirementsascode.act.workflow.WorkflowApi.token;
 
-import java.util.Optional;
 import java.util.function.BiFunction;
 
 import org.requirementsascode.act.core.Data;
@@ -13,29 +13,16 @@ import org.requirementsascode.act.statemachine.Statemachine;
 import org.requirementsascode.act.statemachine.Transition;
 
 public class Flow<T extends ActionData, U extends ActionData> implements ExecutableNode{
-	private final Class<T> dataType;
+	private final Class<T> actionDataType;
 	private final Ports inPorts;
 	private final Ports outPorts;
 	private final BiFunction<WorkflowState, T, U> actionFunction;
 
-	Flow(Class<T> dataType, Ports inPorts, Ports outPorts, BiFunction<WorkflowState, T, U> actionFunction) {
-		this.dataType = requireNonNull(dataType, "actionType must be non-null!");
+	Flow(Class<T> actionDataType, Ports inPorts, Ports outPorts, BiFunction<WorkflowState, T, U> actionFunction) {
+		this.actionDataType = requireNonNull(actionDataType, "actionDataType must be non-null!");
 		this.inPorts = requireNonNull(inPorts, "inPorts must be non-null!");
 		this.outPorts = requireNonNull(outPorts, "outPorts must be non-null!");
 		this.actionFunction = requireNonNull(actionFunction, "actionFunction must be non-null!");
-	}
-
-	@Override
-	public Transition<WorkflowState, Token> asTransition(Statemachine<WorkflowState, Token> owningStatemachine) {
-		return transition(inPorts.asOneState(), outPorts.asOneState(), 
-			inCase(this::tokenHasRightType,this::transformAndMove));
-	}
-	
-	private boolean tokenHasRightType(Data<WorkflowState, Token> inputData) {
-		Optional<ActionData> firstInActionData = inPorts().firstActionData(inputData.state());
-		Class<?> inputDataType = firstInActionData.map(ActionData::getClass)
-			.orElseThrow(() -> new RuntimeException("Unexpected error: no token present in input ports of " + this + "!"));
-		return dataType.isAssignableFrom(inputDataType);
 	}
 	
 	@Override
@@ -47,42 +34,58 @@ public class Flow<T extends ActionData, U extends ActionData> implements Executa
 	public Ports outPorts() {
 		return outPorts;
 	}
+
+	@Override
+	public Transition<WorkflowState, Token> asTransition(Statemachine<WorkflowState, Token> owningStatemachine) {
+		return transition(inPorts.asOneState(), outPorts.asOneState(), 
+			inCase(this::tokenHasRightType,this::transformAndMove));
+	}
+	
+	private boolean tokenHasRightType(Data<WorkflowState, Token> inData) {
+		Class<?> inActionDataType = firstInActionDataType(inData);
+		return actionDataType.isAssignableFrom(inActionDataType);
+	}
+
+	private ActionData firstInActionData(Data<WorkflowState, Token> inData) {
+		return inPorts().firstActionData(inData.state())
+			.orElseThrow(() -> new RuntimeException("Unexpected error: no action data present in input ports of " + this + "!"));
+	}
+	
+	private Class<? extends ActionData> firstInActionDataType(Data<WorkflowState, Token> inData) {
+		return firstInActionData(inData).getClass();
+	}
 	
 	private Data<WorkflowState, Token> transformAndMove(Data<WorkflowState, Token> inputData) {
-		Data<WorkflowState, Token> functionInputData = removeTokenFromInputPorts(inPorts, inputData);
-		Data<WorkflowState, Token> functionOutputData = transform(functionInputData);
-		Data<WorkflowState, Token> outputData = 
-				addTokenToOutputPort(outPorts, functionOutputData);
+		WorkflowState stateAfterRemoval = removeTokenFromInputPorts(inPorts(), inputData.state());
+		Data<WorkflowState, Token> functionOutputData = transform(stateAfterRemoval, firstInActionData(inputData));
+		Data<WorkflowState, Token> outputData = addTokenToOutputPort(outPorts, functionOutputData);
 		return outputData;
 	}
 	
-	private Data<WorkflowState, Token> removeTokenFromInputPorts(Ports inputPorts, Data<WorkflowState, Token> data) {
+	private WorkflowState removeTokenFromInputPorts(Ports inputPorts, WorkflowState state) {
 	    return inputPorts.stream()
-	        .reduce(data, 
-	        	(updatedData, port) -> removeTokenFromInputPort(port, updatedData), 
+	        .reduce(state, 
+	        	(updatedState, port) -> removeTokenFromInputPort(port, updatedState), 
 	        	(data1, data2) -> data2);
 	}
 
-	private Data<WorkflowState, Token> removeTokenFromInputPort(Port<?> inputPort, Data<WorkflowState, Token> data) {
-		WorkflowState state = data.state();
+	private WorkflowState removeTokenFromInputPort(Port<?> inputPort, WorkflowState state) {
 		Token inputToken = state.firstTokenIn(inputPort).get();
 		WorkflowState updatedState = state.removeToken(inputPort, inputToken).state();
-		return data(updatedState, Token.from(data));
+		return updatedState;
 	}
 	
-	private Data<WorkflowState, Token> transform(Data<WorkflowState, Token> data) {
-		U outputActionData = applyActionFunction(data);
-		Token outputToken = Token.from(data).replaceActionData(outputActionData);
-		return data(data.state(), outputToken);
+	@SuppressWarnings("unchecked")
+	private Data<WorkflowState, Token> transform(WorkflowState state, ActionData firstInActionData) {
+		U outActionData = applyActionFunction(state, (T)firstInActionData);
+		return data(state, token(outActionData));
 	}
 	
 	private Data<WorkflowState, Token> addTokenToOutputPort(Ports outPorts, Data<WorkflowState, Token> data) {
 		return data.state().addToken(outPorts.stream().findFirst().get(), Token.from(data));
 	}
 
-	@SuppressWarnings("unchecked")
-	private U applyActionFunction(Data<WorkflowState, Token> inputData) {
-		U outputActionData = actionFunction.apply(inputData.state(), (T) ActionData.from(inputData));
-		return outputActionData;
+	private U applyActionFunction(WorkflowState state, T actionData) {
+		return actionFunction.apply(state, actionData);
 	}
 }
