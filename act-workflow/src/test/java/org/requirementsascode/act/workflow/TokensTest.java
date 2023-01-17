57b61b21fd86a92ac53037554e576bfbb16a0b8c
Bertil Muth <bertilmuth@yahoo.de>
2023-01-17 10:59:04 +0100

    Add test for union of two sets

--- act-workflow/src/test/java/org/requirementsascode/act/workflow/TokensTest.java
+++ act-workflow/src/test/java/org/requirementsascode/act/workflow/TokensTest.java
@@ -1,21 +1,27 @@
 package org.requirementsascode.act.workflow;
 
-import static java.util.Collections.*;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singleton;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.requirementsascode.act.workflow.WorkflowApi.port;
 import static org.requirementsascode.act.workflow.WorkflowApi.token;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.requirementsascode.act.workflow.testdata.StringData;
 
 class TokensTest {
-	private static final String PORT1 = "TestingPort";
+	private static final String PORT1 = "TestingPort1";
+	private static final String PORT2 = "TestingPort2";
 	
 	private static final Token TOKEN_1 = token(new StringData("Token1"));
+	private static final Token TOKEN_2 = token(new StringData("Token2"));
 	
 	@Test
 	void unionOfEmptyTokensIsEmpty() {
@@ -35,6 +41,20 @@ class TokensTest {
 		assertEquals(token1(), union);
 	}
 	
+	@Test
+	void unionOfOneTokenWithItselfIsTheToken() {
+		Tokens union = token1().union(token1());
+		assertEquals(token1(), union);
+	}
+	
+	@Test
+	void unionOfTwoTokens() {
+		Tokens union = token1().union(token2());
+		
+		List<Token> unifiedTokens = asTokenList(union);
+		assertEquals(asList(TOKEN_1, TOKEN_2), unifiedTokens);
+	}
+	
 	private Tokens emptyTokens() {
 		return new Tokens(emptyMap());
 	}
@@ -44,5 +64,16 @@ class TokensTest {
 		HashMap<Port<?>, Set<Token>> oneTokenMap = new HashMap<>();
 		oneTokenMap.put(port1, singleton(TOKEN_1));
 		return new Tokens(oneTokenMap);
+	}
+	
+	private Tokens token2() {
+		Port<?> port1 = port(PORT2, ActionData.class);
+		HashMap<Port<?>, Set<Token>> oneTokenMap = new HashMap<>();
+		oneTokenMap.put(port1, singleton(TOKEN_2));
+		return new Tokens(oneTokenMap);
+	}
+	
+	private List<Token> asTokenList(Tokens union) {
+		return union.asMap().values().stream().flatMap(Set::stream).collect(Collectors.toList());
 	}
 }
