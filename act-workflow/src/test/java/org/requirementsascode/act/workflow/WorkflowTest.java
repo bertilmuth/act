c95a1130355afb6b38dcccaa6363a7bac6002eab
Bertil Muth <bertilmuth@yahoo.de>
2023-01-18 22:34:59 +0100

    Simplify test

--- act-workflow/src/test/java/org/requirementsascode/act/workflow/WorkflowTest.java
+++ act-workflow/src/test/java/org/requirementsascode/act/workflow/WorkflowTest.java
@@ -85,11 +85,10 @@ class WorkflowTest {
 			)
 			.build();
 		
-		WorkflowState state1 = workflow.enterInitialData(action1_In, str(START_WORKFLOW));
-		WorkflowState state2 = workflow.next(state1);
+		WorkflowState state = workflow.enterInitialData(action1_In, str(START_WORKFLOW));
 		
-		assertEquals(str(ACTION1 + "." + ACTION2), action2_Out.firstActionData(state2).get());
-		assertEquals(1, nrOfTokensInState(state2));
+		assertEquals(str(ACTION1 + "." + ACTION2), action2_Out.firstActionData(state).get());
+		assertEquals(1, nrOfTokensInState(state));
 	}
 	
 	@Test
