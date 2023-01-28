f475c09995be982868ee022a4907d3736f73131c
Bertil Muth <bertilmuth@yahoo.de>
2023-01-28 09:03:50 +0100

    Make Action depend on PartBehavior

--- act-workflow/src/main/java/org/requirementsascode/act/workflow/Action.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/Action.java
@@ -3,27 +3,23 @@ package org.requirementsascode.act.workflow;
 import static java.util.Objects.requireNonNull;
 import static org.requirementsascode.act.workflow.WorkflowApi.ports;
 
-import java.util.function.BiFunction;
-
-import org.requirementsascode.act.workflow.function.Apply;
+import org.requirementsascode.act.workflow.function.PartBehavior;
 
 public class Action<T extends ActionData, U extends ActionData> implements Named, Part{
 	private final String name;
 	private final Ports inPorts;
 	private final Ports outPorts;
-	private final Apply<T,U> apply;
+	private final PartBehavior partBehavior;
 
-	Action(String actionName, Port<T> inPort, Port<U> outPort, Class<T> inputType, BiFunction<WorkflowState, T, U> actionFunction) {
-		this(actionName, ports(inPort), ports(outPort), inputType, actionFunction);
+	Action(String actionName, Port<T> inPort, Port<U> outPort, PartBehavior partBehavior) {
+		this(actionName, ports(inPort), ports(outPort), partBehavior);
 	}
 	
-	Action(String actionName, Ports inPorts, Ports outPorts, Class<T> inputType, BiFunction<WorkflowState, T, U> actionFunction) {
+	Action(String actionName, Ports inPorts, Ports outPorts, PartBehavior partBehavior) {
 		this.name = requireNonNull(actionName, "actionName must be non-null!");	
 		this.inPorts = requireNonNull(inPorts, "inPorts must be non-null!");	
 		this.outPorts = requireNonNull(outPorts, "outPorts must be non-null!");	
-		
-		requireNonNull(actionFunction, "actionFunction must be non-null!");
-		this.apply = new Apply<>(inputType, actionFunction);	
+		this.partBehavior = requireNonNull(partBehavior, "partBehavior must be non-null!");
 	}
 
 	@Override
@@ -43,7 +39,7 @@ public class Action<T extends ActionData, U extends ActionData> implements Named
 	
 	@Override
 	public Flow asFlow() {
-		return new Flow(this, apply);
+		return new Flow(this, partBehavior);
 	}
 
 	@Override
--- act-workflow/src/main/java/org/requirementsascode/act/workflow/WorkflowApi.java
+++ act-workflow/src/main/java/org/requirementsascode/act/workflow/WorkflowApi.java
@@ -3,17 +3,21 @@ package org.requirementsascode.act.workflow;
 import java.util.Arrays;
 import java.util.function.BiFunction;
 
+import org.requirementsascode.act.workflow.function.Apply;
+
 public class WorkflowApi {
 	public static <T extends ActionData> Port<T> port(String portName, Class<T> portType){
 		return new Port<>(portName, portType);
 	}
 	
 	public static <T extends ActionData, U extends ActionData> Action<T,U> action(String actionName, Port<T> inputPort, Port<U> outputPort, Class<T> actionType, BiFunction<WorkflowState, T, U> actionFunction) {
-		return new Action<>(actionName, inputPort, outputPort, actionType, actionFunction);
+		Apply<T,U> apply = new Apply<>(actionType, actionFunction);
+		return new Action<>(actionName, inputPort, outputPort, apply);
 	}
 	
 	public static <T extends ActionData, U extends ActionData> Action<T,U> action(String actionName, Ports inputPorts, Ports outputPorts, Class<T> actionType, BiFunction<WorkflowState, T, U> actionFunction) {
-		return new Action<>(actionName, inputPorts, outputPorts, actionType, actionFunction);
+		Apply<T,U> apply = new Apply<>(actionType, actionFunction);
+		return new Action<>(actionName, inputPorts, outputPorts, apply);
 	}
 	
 	public static Token emptyToken() {
