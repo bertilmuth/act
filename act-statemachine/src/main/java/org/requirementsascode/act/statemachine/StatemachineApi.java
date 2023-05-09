6202c22e3db460567c5fe9e9e128d6a7bbe3eb43
Bertil Muth <bertilmuth@yahoo.de>
2023-05-09 14:31:03 +0200

    Use constructor for MapWith

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/StatemachineApi.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/StatemachineApi.java
@@ -61,7 +61,7 @@ public class StatemachineApi {
 	}
 	
 	public static <S,V1,V2> Behavior<S,V1,V2> mapWith(BiFunction<S,V1,Data<S,V2>> mapper){
-		return MapWith.mapWith(mapper);
+		return new MapWith<>(mapper);
 	}
 	
 	public static <S,V> Behavior<S,V,V> init(Function<V,S> init){
--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/function/MapWith.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/function/MapWith.java
@@ -7,16 +7,19 @@ import java.util.function.BiFunction;
 import org.requirementsascode.act.core.Behavior;
 import org.requirementsascode.act.core.Data;
 
-public class MapWith{
-	public static <S,V1,V2> Behavior<S,V1,V2> mapWith(BiFunction<S,V1,Data<S,V2>> mapper){
-		requireNonNull(mapper, "mapper must be non-null!");
+public class MapWith<S,V1,V2> implements Behavior<S,V1,V2>{
+	private final BiFunction<S, V1, Data<S, V2>> mapper;
+
+	public MapWith(BiFunction<S,V1,Data<S,V2>> mapper){
+		this.mapper = requireNonNull(mapper, "mapper must be non-null!");
+	}
+
+	@Override
+	public Data<S, V2> actOn(Data<S, V1> d) {
+		assert(d.value().isPresent());
+		V1 trigger = d.value().get();
+		Data<S, V2> newData = mapper.apply(d.state(), trigger);
 		
-		return d -> {
-			assert(d.value().isPresent());
-			V1 trigger = d.value().get();
-			Data<S, V2> newData = mapper.apply(d.state(), trigger);
-			
-			return newData;
-		};
+		return newData;
 	}
 }
