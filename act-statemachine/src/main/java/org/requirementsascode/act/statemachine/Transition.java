76ac89174c4134c2f18b54a67534b32ccc826340
Bertil Muth <bertilmuth@yahoo.de>
2023-05-19 09:11:49 +0200

    Move CheckedEntryBehaviorSupplier to Transition

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Transition.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Transition.java
@@ -3,8 +3,10 @@ package org.requirementsascode.act.statemachine;
 import static java.util.Objects.requireNonNull;
 import static org.requirementsascode.act.core.InCase.inCase;
 
+import java.util.function.Function;
+
 import org.requirementsascode.act.core.Behavior;
-import org.requirementsascode.act.core.Data;
+import org.requirementsascode.act.statemachine.Transition.ToStateEntryBehaviorSupplier;
 
 public class Transition<S, V0> implements Behavioral<S,V0>, Transitionable<S, V0> {
 	private final State<S, V0> fromState;
@@ -56,5 +58,29 @@ public class Transition<S, V0> implements Behavioral<S,V0>, Transitionable<S, V0
 	
 	public interface ToStateEntryBehaviorSupplier<S,V0>{
 		Behavior<S,V0,V0> supply(Statemachine<S, V0> statemachine, Transition<S,V0> transition);
+	}
+}
+
+class CheckedEntryBehaviorSupplier<S,V0> implements ToStateEntryBehaviorSupplier<S,V0>{
+	private final Function<Statemachine<S, V0>, Behavior<S, V0, V0>> toStateBehaviorSupplier;
+
+	public CheckedEntryBehaviorSupplier(Function<Statemachine<S,V0>, Behavior<S,V0,V0>> toStateBehaviorSupplier) {
+		this.toStateBehaviorSupplier = toStateBehaviorSupplier;
+	}
+	
+	@Override
+	public Behavior<S, V0, V0> supply(Statemachine<S, V0> sm, Transition<S, V0> transition) {
+		State<S, V0> fromState = transition.fromState();
+		State<S, V0> toState = transition.toState();
+		Behavior<S, V0, V0> toStateBehavior = toStateBehaviorSupplier.apply(sm);
+		
+		return new TriggeredBehavior<>(inCase(toState::matchesStateIn, 
+			toStateBehavior, errorIfNotInToState(fromState, toState)));
+	}
+	
+	private Behavior<S, V0,V0> errorIfNotInToState(State<S,V0> fromState, State<S,V0> toState) {
+		return d -> {
+			throw new IllegalStateException("Tried transition from " + fromState + " to " + toState + ", but invariant was false in toState! Data: " + d);
+		};
 	}
 }
