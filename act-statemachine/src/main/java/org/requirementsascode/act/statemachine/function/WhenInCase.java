86fd34a6703e3780074efbd37922074f391b6268
Bertil Muth <bertilmuth@yahoo.de>
2023-05-31 00:09:37 +0200

    Refactor WhenInCase

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/function/WhenInCase.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/function/WhenInCase.java
@@ -11,34 +11,38 @@ import org.requirementsascode.act.core.Data;
 public class WhenInCase<S,V0> implements Behavior<S,V0,V0> {
 	private Behavior<S, V0, V0> whenInCase;
 
-	@SuppressWarnings("unchecked")
 	public <V1 extends V0, V2 extends V0> WhenInCase(Class<V1> expectedType, Predicate<Data<S,V1>> predicate, Behavior<S, V1,V2> behavior) {
 		requireNonNull(expectedType, "expectedType must be non-null!");
 		requireNonNull(behavior, "behavior must be non-null!");		
-		this.whenInCase = (Behavior<S, V0,V0>) inCase(typeMatches(expectedType), inCase(predicate, i -> behaviorActOn(i, behavior)));
+		Behavior<S,V0,V0> conditionalBehavior = inCase(predicateV0(predicate), behaviorV0(behavior));
+		this.whenInCase = inCase(typeMatches(expectedType), conditionalBehavior);
 	}
 	
 	@Override
 	public Data<S, V0> actOn(Data<S, V0> d) {
 		return whenInCase.actOn(d);
 	}
+	
+	@SuppressWarnings("unchecked")
+	private <V1 extends V0> Predicate<Data<S,V0>> predicateV0(Predicate<Data<S,V1>> predicate){
+		return d -> predicate.test((Data<S,V1>)d);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private <V1 extends V0, V2 extends V0> Behavior<S,V0,V0> behaviorV0(Behavior<S, V1,V2> behavior){
+		return d -> (Data<S,V0>)behavior.actOn((Data<S,V1>)d);
+	}
 
 	private static <S, V1 extends V0, V0> Predicate<Data<S, V0>> typeMatches(Class<V1> expectedType) {
-		Predicate<Data<S, V0>> predicate = d -> d.value().isPresent() && hasExpectedType(triggerTypeOf(d), expectedType);
+		Predicate<Data<S, V0>> predicate = d -> d.value().isPresent() && hasExpectedType(valueTypeOf(d), expectedType);
 		return predicate;
 	}
 
-	@SuppressWarnings("unchecked")
-	private static <S, V1 extends V0, V2 extends V0, V0> Data<S, V0> behaviorActOn(Data<S, V1> before, Behavior<S, V1,V2> behavior) {
-		Data<S, V0> after = (Data<S, V0>) behavior.actOn(before);
-		return after;
+	private static boolean hasExpectedType(Class<?> valueType, Class<?> expectedType) {
+		return expectedType.isAssignableFrom(valueType);
 	}
 
-	private static <V> boolean hasExpectedType(Class<? extends Object> beforeType, Class<V> expectedType) {
-		return expectedType.isAssignableFrom(beforeType);
-	}
-
-	private static Class<? extends Object> triggerTypeOf(Data<?, ?> d) {
+	private static Class<?> valueTypeOf(Data<?, ?> d) {
 		return d.value().get().getClass();
 	}
 }
