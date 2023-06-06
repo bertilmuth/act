1e4d6706bedf53984088c01b232bca1c08dd4092
Bertil Muth <bertilmuth@yahoo.de>
2023-06-06 23:55:21 +0200

    Remove hasActed from InCase

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/function/WhenInCase.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/function/WhenInCase.java
@@ -1,7 +1,6 @@
 package org.requirementsascode.act.statemachine.function;
 
 import static java.util.Objects.requireNonNull;
-import static org.requirementsascode.act.core.Behavior.hasActed;
 import static org.requirementsascode.act.core.InCase.inCase;
 
 import java.util.function.Predicate;
@@ -35,7 +34,7 @@ public class WhenInCase<S,V0> implements Behavior<S,V0,V0> {
 	}
 
 	private static <S, V1 extends V0, V0> Predicate<Data<S, V0>> typeMatches(Class<V1> expectedType) {
-		Predicate<Data<S, V0>> predicate = d -> hasActed(d) && hasExpectedType(valueTypeOf(d), expectedType);
+		Predicate<Data<S, V0>> predicate = d -> hasExpectedType(valueTypeOf(d), expectedType);
 		return predicate;
 	}
 
