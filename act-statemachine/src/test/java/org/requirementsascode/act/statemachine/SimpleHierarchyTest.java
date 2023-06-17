44772add63ab4d9631e7b6a4a3ea4ff876f60bb2
Bertil Muth <bertilmuth@yahoo.de>
2023-06-17 08:06:35 +0200

    Add simple hierarchical test

---
+++ act-statemachine/src/test/java/org/requirementsascode/act/statemachine/SimpleHierarchyTest.java
@@ -0,0 +1,53 @@
+package org.requirementsascode.act.statemachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.requirementsascode.act.core.Data;
+
+import static org.requirementsascode.act.statemachine.StatemachineApi.*;
+
+class SimpleHierarchyTest {
+	private static final String TOP_S1 = "top_s1";
+	private static final String TOP_S2 = "top_s2";
+	
+	interface Trigger{ }
+	class Switch implements Trigger{ }
+
+	private Statemachine<String, Trigger> top;
+
+	@BeforeEach
+	void test() {
+		State<String, Trigger> top_s1 = state(TOP_S1, s -> s != null && s.equals(TOP_S1));
+		State<String, Trigger> top_s2 = state(TOP_S2, s -> s != null && s.equals(TOP_S2));
+		
+		top =
+			Statemachine.builder()
+				.states(
+					top_s1,
+					top_s2
+				)
+				.transitions(
+					entryFlow(top_s1, consumeWith((s,v) -> TOP_S1)),
+					transition(top_s1, top_s2, when(Switch.class, consumeWith((s,v) -> TOP_S2)))
+				)
+				.build();
+
+	}
+
+	@Test
+	void switchesOnce() {
+		Data<String, Trigger> afterInit = topInit();
+		assertEquals(TOP_S2, topActOn(afterInit, new Switch()));
+	}
+
+	private String topActOn(Data<String, Trigger> before, Trigger trigger) {
+		Data<String, Trigger> d = data(before.state(), trigger);
+		return top.actOn(d).state();
+	}
+
+	private Data<String, Trigger> topInit() {
+		return top.actOn(data(null, null));
+	}
+}
