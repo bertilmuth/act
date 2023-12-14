02995ed48a52515e75cb91fc987fa7a5ddc1848e
Bertil Muth <bertilmuth@yahoo.de>
2023-12-14 14:28:12 +0100

    Fix toolchain and include check in Transition

--- act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Transition.java
+++ act-statemachine/src/main/java/org/requirementsascode/act/statemachine/Transition.java
@@ -52,7 +52,8 @@ public class Transition<S, V0> implements Behavioral<S,V0>, Transitionable<S, V0
 		Behavior<S, V0, V0> checkedToStateEntryBehavior = createCheckedToStateEntryBehavior(toStateEntryBehavior);	
 		
 		return inCase(fromState()::matchesStateIn,
-			transitionBehavior().andThen(checkedToStateEntryBehavior));
+			transitionBehavior().andThen(
+				inCase(Behavior::hasActed, checkedToStateEntryBehavior)));
 	}
 	
 	private Behavior<S, V0, V0> createCheckedToStateEntryBehavior(Behavior<S, V0, V0> toStateEntryBehavior) {
